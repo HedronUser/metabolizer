b1b2e743c5ea41d7415306406ee6891d651be9d3
Jesse Jenkins <jessetjenkins@gmail.com>
2018-09-19 12:51:11 -0700

    working temperature reading on the esp device. Added the mac driver for the cheap chinese ESP device

--- serialthermocouple/serialthermocouple.ino
+++ serialthermocouple/serialthermocouple.ino
@@ -3,19 +3,19 @@
 
 #include "max6675.h"
 
-int thermoDO = 4;
-int thermoCS = 5;
-int thermoCLK = 6;
+int thermoDO = 12;
+int thermoCS = 13;
+int thermoCLK = 14;
 
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 int vccPin = 3;
 int gndPin = 2;
   
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   // use Arduino pins 
-  pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
-  pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
+  //pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
+  //pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
   
   Serial.println("MAX6675 test");
   // wait for MAX chip to stabilize
